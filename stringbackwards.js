016dd1f61e18d8bdccd32b78db8a53989322303c
Fabian Leon <87119033+fabianleon1@users.noreply.github.com>
2022-10-10 20:16:30 -0500

    Delete stringbackwards.js

--- stringbackwards.js
+++
@@ -1,9 +0,0 @@
-// This function convert a string backwards
-
-const backwards= (nombre) =>{
-       for(var i=nombre.length-1; i>=0; i--)
-       {
-              console.log(nombre[i]);
-       }
-}
-backwards("John");
